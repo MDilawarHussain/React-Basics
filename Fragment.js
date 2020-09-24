35fa274570c3439f8c3195c9b67d96a2aa3581f9
Dilawar Hussain <71829167+MDilawarHussain@users.noreply.github.com>
2020-09-24 06:49:16 -0700

    Render Multiple Elements In React Using Fragment

---
+++ Fragment.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <React.Fragment>
+<h1>
+  A
+</h1>
+<h2> B </h2>
+<h3>
+  C
+</h3>
+
+
+  </React.Fragment>
+  ,
+  document.getElementById("root")
+
+
+
+
+);
