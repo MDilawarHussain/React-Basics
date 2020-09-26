ba5ac53a806fc9b36fe98ed279b3d94c00c1c46b
Dilawar Hussain <71829167+MDilawarHussain@users.noreply.github.com>
2020-09-25 22:53:31 -0700

    Show Or Access Variable In Render Method , JSX Expression

---
+++ Variable,Expression.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const Name = "Dilawar Hussain";
+
+ReactDom.render(
+  <>
+  <h1> My Name Is {Name}    </h1>
+  <h3>The Num Is {9*8}</h3>
+
+
+  </>,
+  document.getElementById("root")
+
+
+);
