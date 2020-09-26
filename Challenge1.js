dbba6d5b7e0647d6af9f044a3146484e29a7e5e3
Dilawar Hussain <71829167+MDilawarHussain@users.noreply.github.com>
2020-09-25 22:52:32 -0700

    Simple Challenge

---
+++ Challenge1.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <>
+
+  <h1>NETFLIX PICK</h1>
+  <h2>List Of Best 5 Netflix Series:</h2>
+  <h3>
+    1. Dark <br/>
+    2. Peaky Blinders<br/>
+    3. Game Of Throne<br/>
+    4. Jassica Jones<br/>
+    5. The Protector<br/>
+  </h3>
+  </>,
+  document.getElementById("root")
+);
